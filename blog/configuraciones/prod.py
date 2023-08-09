0711096fd78646b9d842fa6de59152cb19af2f0e
carloslionelxd <carlos.lionel.barboza@gmail.com>
2023-08-08 23:51:09 -0300

    Primer posteo

---
+++ blog/configuraciones/prod.py
@@ -0,0 +1,15 @@
+from .settings import *
+
+
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AlanSuperuser$default',
+        'USER': 'AlanSuperuser',
+        'PASSWORD': '4dmin1239',  #Alan #F3rn3t4340.A #4dmin1239 #MySQL
+        'HOST': 'AlanSuperuser.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
+'''
